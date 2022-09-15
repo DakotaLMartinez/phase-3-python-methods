592fc284482b84a16d623ee6402b8056bf214575
Dakota L Martinez <dakotaleemusic@gmail.com>
2022-09-15 16:06:38 -0700

    solution to lesson 5

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    """
+    greet_programmer() prints "Hello, programmer!"
+    """
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    '''greet(name) accepts a name and includes it in a greeting'''
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    '''accepts an optional name and includes it in a greeting'''
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    '''accepts 2 numbers and returns their sum'''
+    return num1 + num2
 
 def halve(number):
-    pass
+    '''accepst a number as an argument and returns half its value'''
+    return number/2
